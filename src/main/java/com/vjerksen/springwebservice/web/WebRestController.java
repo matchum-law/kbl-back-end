096cb2595337fa567773f7e4c3fb2ef47aa15eeb
vjerksen <md040207@naver.com>
2019-06-14 22:02:42 +0900

    first commit

---
+++ src/main/java/com/vjerksen/springwebservice/web/WebRestController.java
@@ -0,0 +1,25 @@
+package com.vjerksen.springwebservice.web;
+
+import com.vjerksen.springwebservice.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+}
