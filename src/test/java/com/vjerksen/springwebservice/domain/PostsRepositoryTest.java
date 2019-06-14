638ebca919fc5bd9257598619e90d0daffe6c8b6
vjerksen <md040207@naver.com>
2019-06-14 22:59:12 +0900

    remove imports not use

--- src/test/java/com/vjerksen/springwebservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/vjerksen/springwebservice/domain/PostsRepositoryTest.java
@@ -8,11 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.jvm.hotspot.utilities.Assert;
 
 import java.util.List;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
