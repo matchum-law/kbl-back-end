98f7ea8539e377466b137bbeb2b6b140e44f139a
vjerksen <md040207@naver.com>
2019-06-15 00:18:11 +0900

    hello

--- src/main/java/com/vjerksen/springwebservice/web/WebRestController.java
+++ src/main/java/com/vjerksen/springwebservice/web/WebRestController.java
@@ -2,13 +2,11 @@ package com.vjerksen.springwebservice.web;
 
 import com.vjerksen.springwebservice.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 public class WebRestController {
 
     private PostsRepository postsRepository;
